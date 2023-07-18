8faf816edf17edf444f58e19cd98fb14f6d11175
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-18 14:59:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let character = null
 function firstNonRepeatingCharacter(string) {
 	for(let i = 0;i<string.length;i++){
 		
-		if(string.indexOf(string[i])==i){
+		if(string.lastIndexOf(string[i])==i && string.indexOf(string[i])==i){
 			 character = string[i];
 			return;
 			
