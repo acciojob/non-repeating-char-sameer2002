34069ad1906d20131be4a012e2d39a3a384d97fb
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-18 14:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let givenString = prompt("Enter a string: ");
+let character = null
+function firstNonRepeatingCharacter(string) {
+	for(let i = 0;i<string.length;i++){
+		
+		if(string.indexOf(string[i])==i){
+			 character = string[i];
+			return;
+			
+		}
+	}	
+}
+firstNonRepeatingCharacter(givenString);
+alert(character);
